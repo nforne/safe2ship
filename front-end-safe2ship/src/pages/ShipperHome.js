41435b6610fea031ae1bdcfda1c1348d58d8e9a5
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2022-01-22 22:32:57 -0500

    Add placeholder code to dynamically display package list

--- front-end-safe2ship/src/pages/ShipperHome.js
+++ front-end-safe2ship/src/pages/ShipperHome.js
@@ -2,17 +2,14 @@ import React from "react";
 import PackageListItem from "../components/PackageListItem";
 
 export default function ShipperHome(props) {
+  const packageList =  []; 
+  for (let index = 0; index < 5; index++) {
+    packageList.push(<PackageListItem />);
+  }
+  
   return (
     <div class="container">
-      <PackageListItem />
-      <PackageListItem />
-
-      <PackageListItem />
-
-      <PackageListItem />
-
-      <PackageListItem />
-
+      { packageList }
     </div>
   );
 }
