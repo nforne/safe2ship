9872b7c6b90a2284e2a9bef0472f467b2fe13d7e
Abdullah S. Khan <abdullahshamskhan@gmail.com>
2022-01-22 23:13:19 -0500

    Add Customer Home Page

---
+++ front-end-safe2ship/src/pages/CustomerHome.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function CustomerHome(props) {
+  return (
+    <div class="row justify-content-between">
+      <div class="col">
+        <h2>My Packages</h2>
+      </div>
+    </div>
+  );
+}
